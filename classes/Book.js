8c4168452e7d0815140f39c6e10022a013173ceb
madisondegrezia <madisondegrezia26@gmail.com>
2025-05-06 12:49:20 -0400

    Created and implemented the Media and Book classes

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,44 @@
 // import the Media class:
+const Media = require("./Media.js");
 
 // create your Book class:
+class Book extends Media {
+  constructor(title, year, genre, author, numPages, rating) {
+    super(title, year, genre);
+    this.author = author;
+    this.numPages = numPages;
+    // rating must be a number between 1-5
+    if (rating < 1 || rating > 5) {
+      throw new Error("Rating must be a number between 1-5");
+    }
+    this.rating = rating;
+  }
 
+  summary() {
+    return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+  }
+
+  static highestRating(bookArr) {
+    if (!bookArr || bookArr.length === 0) {
+      return undefined;
+    }
+    let highest = bookArr[0];
+    for (let i = 1; i < bookArr.length; i++) {
+      if (bookArr[i].rating > highest.rating) {
+        highest = bookArr[i];
+      }
+    }
+    return highest;
+  }
+
+  static calculateAverageRating(bookArr) {
+    let sum = 0;
+    for (let i = 0; i < bookArr.length; i++) {
+      sum += bookArr[i].rating;
+    }
+
+    return (sum / bookArr.length).toFixed(2);
+  }
+}
 // don't change below
 module.exports = Book;
--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,21 @@
 // create your Media class:
+class Media {
+  static totalMediaCount = 0;
+  // an array that contains all Media objects created using the class
+  static ALL_MEDIA = [];
+
+  constructor(title, year, genre) {
+    Media.totalMediaCount++;
+    Media.ALL_MEDIA.push(this);
+    this.title = title;
+    this.year = year;
+    this.genre = genre;
+  }
+  // tracks how many Media items have been created
+  summary() {
+    return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}`;
+  }
+}
 
 // uncomment below to export it:
 module.exports = Media;
