3ebbc877a72828ba353298761702744e055c5619
Lauren Balch <llbalch@gmail.com>
2025-05-06 11:58:02 -0500

    Completed Music and Movie classes

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,34 @@
 // import the Media class:
+const Media = require('./Media.js');
 
 // create your Movie class:
+class Movie extends Media{
+    constructor(title, year, genre, director, duration, rating){
+        super(title, year, genre)
+        this.director = director;
+        this.duration = duration;
+        if (rating < 1 || rating > 5){
+            throw new Error('Rating must be between 1 and 5')
+        } else {
+            this.rating = rating;}
+}
+    summary(){
+        return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Duration:${this.duration}, Rating: ${this.rating}`;
+    }
 
+    static longestMovie(movies){
+        return movies.reduce((longest, movie) => {
+            return (movie.duration > longest.duration) ? movie : longest;
+        });
+    }
+
+    static calculateAverageRating(movies){
+        let sum = 0;
+        for (let i = 0; i < movies.length; i++){
+            sum += movies[i].rating;
+        }
+        return (sum / movies.length).toFixed(2);
+    }
+}
 // don't change below
 module.exports = Movie;
--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,22 @@
 // import the Media class:
+const Media = require('./Media.js');
 
 // create your Music class:
+class Music extends Media{
+    constructor(title, year, genre, artist, length) {
+        super(title, year, genre)
+        this.artist = artist;
+        this.length = length;
+    }
+    summary() {
+        return `Title: ${this.title}, Artist: ${this.artist}, Year ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`;
+    }
+    static shortestAlbum(music){
+        return music.reduce((shortest, album) => {
+            return (album.length < shortest.length) ? album : shortest;
+        });
+    }
+}
 
 // don't change below
 module.exports = Music;
